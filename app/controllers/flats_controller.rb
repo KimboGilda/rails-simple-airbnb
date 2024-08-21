07cb7533eabe56a5b42e2042156b59704a2888d2
nikos bakogiannis <nbakojohn13@gmail.com>
2024-08-21 19:24:03 +0200

    add some more

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,13 +2,27 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+  
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(restaurant_params)
+    if @flat.save
+        redirect_to @flat, notice: "Flat was successfully created."
+      else
+        render :new, status: :unprocessable_entity
+      end
+
+  end
 
   def show
   end
 
-  def new
+  private
+  def restaurant_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 
-  def create
-  end
 end
